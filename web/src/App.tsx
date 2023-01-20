9d056fcd5dae0d35ceb1d5b4720b85084e22037e
azanniel <leo.azannielttt@gmail.com>
2023-01-20 02:36:37 -0400

    🤡 add connection with backend using axios lib

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import './lib/dayjs';
 import './styles/global.css';
 
 import { Header } from './components/Header';
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,8 +1,46 @@
+import { FormEvent, useState } from 'react';
 import { Check } from 'phosphor-react';
+import * as Checkbox from '@radix-ui/react-checkbox';
+
+import { api } from '../lib/axios';
+
+const availableWeekDays = [
+  'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira',
+  'Sexta-feira', 'Sábado'
+];
 
 export function NewHabitForm() {
+  const [title, setTitle] = useState('');
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  async function createNewHabit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if(!title || weekDays.length === 0) {
+      return;
+    }
+
+    await api.post('/habits', {
+      title,
+      weekDays
+    });
+
+    setTitle('');
+    setWeekDays([]);
+
+    alert('Hábito criado com sucesso!');
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if(weekDays.includes(weekDay)) {
+      setWeekDays(prevState => prevState.filter(day => day !== weekDay));
+    }else {
+      setWeekDays(prevState => [...prevState, weekDay]);
+    }
+  }
+
   return (
-    <form className='w-full flex flex-col mt-6'>
+    <form onSubmit={createNewHabit} className='w-full flex flex-col mt-6'>
       <label htmlFor='title' className='font-semibold leading-tight'>
         Qual seu comprometimento?
       </label>
@@ -13,11 +51,36 @@ export function NewHabitForm() {
         id='title'
         placeholder='ex.: Execícios, dormir bem, etc...'
         autoFocus
+        onChange={event => setTitle(event.target.value)}
+        value={title}
       />
 
       <label className='font-semibold leading-tight mt-4'>
         Qual a recorrência?
       </label>
+
+      <div className='mt-3 flex flex-col gap-2'>
+        {availableWeekDays.map((weekDay, index) => {
+          return (
+            <Checkbox.Root
+              key={weekDay}
+              className='flex items-center gap-3 group'
+              checked={weekDays.includes(index)}
+              onCheckedChange={() => handleToggleWeekDay(index)}
+            >
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                <Checkbox.Indicator>
+                  <Check size={20} className='text-white' />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className='leading-tight text-white'>
+                {weekDay}
+              </span>
+            </Checkbox.Root>
+          )
+        })}
+      </div>
 
       <button
         className='mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors'
